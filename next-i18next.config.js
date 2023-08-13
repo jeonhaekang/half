16960c397e62e04b0f02e7642469b508687c5d58
jeonhaekang <1003gorkd@naver.com>
2023-08-14 02:22:52 +0900

    ㅠㅠ

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+/* eslint-disable  */
+const path = require("path");
+
 module.exports = {
   i18n: {
     locales: ["ko", "jp"],
-    defaultLocale: "ko"
+    defaultLocale: "ko",
+    localePath: path.resolve("./public/locales")
   }
 };
