8d571c3e62eb2c8a6029fd1eb875569b50d009b0
jeonhaekang <1003gorkd@naver.com>
2023-08-14 02:08:44 +0900

    -

--- src/components/Shared/Header/Header.tsx
+++ src/components/Shared/Header/Header.tsx
@@ -1,18 +1,40 @@
 import { useTranslation } from "next-i18next";
-import { Toggle } from "~/components/Commons";
+import { useRouter } from "next/router";
+import { Button, Toggle } from "~/components/Commons";
 import { useThemeStore } from "~/states/client";
-import { Text } from "~/styles/mixins";
+import { FlexCenter, Text } from "~/styles/mixins";
 import * as Styled from "./Header.styles";
 
 export const Header = () => {
   const { theme, toggle } = useThemeStore();
   const { t } = useTranslation("common");
+  const router = useRouter();
 
   return (
     <Styled.Header>
       <Text>{t("title")}</Text>
 
-      <Toggle checked={theme} onChange={toggle} />
+      <FlexCenter gap={12}>
+        <Button
+          size="small"
+          onClick={() => {
+            router.push(router.pathname, router.pathname, { locale: "ko" });
+          }}
+        >
+          한국어
+        </Button>
+
+        <Button
+          size="small"
+          onClick={() => {
+            router.push(router.pathname, router.pathname, { locale: "jp" });
+          }}
+        >
+          日本語
+        </Button>
+
+        <Toggle checked={theme} onChange={toggle} />
+      </FlexCenter>
     </Styled.Header>
   );
 };
